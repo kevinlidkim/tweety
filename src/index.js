a3663281a68f2715ad9b38e15ab0cf31915ddb76
kevin <kevin.li.3@stonybrook.edu>
2017-03-16 23:35:28 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world</h1>
+  </div>,
+  document.getElementById('root')
+);
