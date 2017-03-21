4aef09f5f6888338f34ec851d8fe092c167265a1
kevin <kevin.li.3@stonybrook.edu>
2017-03-21 03:15:06 -0400

    looks weird on ubuntu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends React.Component {
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/signup">Sign Up</Link></li>
             <li><Link to="/profile">Profile</Link></li>
-            <li onClick={this.logoutEvent.bind(this)}><Link>Logout</Link></li>
+            <li onClick={this.logoutEvent.bind(this)}><Link to="">Logout</Link></li>
           </ul>
         </div>
       </nav>
