faf81e561d49a0c478a6b96a613ce2aa7635ca91
kevin <kevin.li.3@stonybrook.edu>
2017-03-22 12:59:44 -0400

    took out email

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -22,6 +22,81 @@ var authenticate = function(password, salt, hashed_password) {
 }
 
 
+exports.add_user_email = function(req, res) {
+
+  if (db.get() == null) {
+    return res.status(500).json({
+      status: 'error',
+      error: 'Database error'
+    })
+  }
+
+  var collection = db.get().collection('users');
+  collection.findOne({
+    $or: [{ email: req.body.email }, { username: req.body.username }]
+  })
+    .then(function(user) {
+      if (user) {
+        return res.status(500).json({
+          status: 'error',
+          error: 'Email or username already in use'
+        })
+      } else {
+        var salt = make_salt();
+        var hashed_password = encrypt_password(req.body.password, salt);
+        var random_key = encrypt_password(make_salt(), make_salt());
+        collection.insert({
+          username: req.body.username,
+          email: req.body.email,
+          salt: salt,
+          hashed_password: hashed_password,
+          verified: false,
+          random_key: random_key
+        })
+          .then(function(data) {
+
+            var transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                user: 'noreplyeliza@gmail.com',
+                pass: 'cse356!@'
+              }
+            });
+
+            var mail_options = {
+              from: '"Eliza 👻" <noreplyeliza@gmail.com>', // sender address
+              to: req.body.email, // list of receivers
+              subject: 'Eliza Verification ✔', // Subject line
+              text: random_key, // plain text body
+              html: '<b>' + random_key + '</b>' // html body
+            };
+
+            transporter.sendMail(mail_options, (error, info) => {
+              if (!error) {
+                return res.status(200).json({
+                  status: 'OK',
+                  message: 'Successfully created user'
+                })
+              } else {
+                return res.status(500).json({
+                  status: 'error',
+                  error: 'Unable to send email'
+                })
+              }
+            });
+          })
+          .catch(function(err) {
+            console.log(err);
+            return res.status(500).json({
+              status: 'error',
+              error: 'Error creating user'
+            })
+          })
+      }
+    })
+
+}
+
 exports.add_user = function(req, res) {
 
   if (db.get() == null) {
@@ -360,9 +435,6 @@ exports.search_items = function(req, res) {
           timestamp: item.timestamp
         }
         items.push(result);
-        console.log("ITS BEING CALLED ==================================");
-        console.log(result);
-        console.log("DONE CALL =====================================")
       })
       return res.status(200).json({
         status: 'OK',
