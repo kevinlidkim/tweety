53227295a1cd110a14434d346b5a24817e0f649b
kevin <kevin.li.3@stonybrook.edu>
2017-03-23 00:17:28 -0400

    10000 minutes session now

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use(cookieParser());
-app.use(session({resave: true, saveUninitialized: true, secret: 'supersecretfriedchicken', cookie: { maxAge: 6000000 }}));
+app.use(session({resave: true, saveUninitialized: true, secret: 'supersecretfriedchicken', cookie: { maxAge: 600000000 }}));
 
 app.use('/static', express.static(__dirname + '/public')); // need this to read bundle.js
 require('./server/routes')(app);
