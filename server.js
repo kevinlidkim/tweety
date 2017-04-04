dadcc94242b691744131c035c9a2be7ddab79b71
kevin <kevin.li.3@stonybrook.edu>
2017-04-04 12:26:59 -0400

    production ready

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var app = express();
 var bodyParser     = require('body-parser');
 var session        = require('express-session');
 var cookieParser   = require('cookie-parser');
+var MongoStore = require('connect-mongo')(session);
 
 var port = process.env.PORT || 80;
 
 var db = require('./db');
-var mongo_uri = 'mongodb://localhost:27017/tweety';
+var mongo_uri = 'mongodb://192.168.1.13:27017/tweety';
 
 db.connect(mongo_uri, function(err) {
   if (err) {
@@ -17,11 +18,17 @@ db.connect(mongo_uri, function(err) {
   }
 })
 
+
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use(cookieParser());
-app.use(session({resave: true, saveUninitialized: true, secret: 'supersecretfriedchicken', cookie: { maxAge: 600000000 }}));
+app.use(session({resave: true, 
+                 saveUninitialized: true, 
+                 secret: 'supersecretfriedchicken', 
+                 cookie: { maxAge: 1000 * 60 * 60 * 2 },
+                 store: new MongoStore({ url: mongo_uri })
+               }));
 
 app.use('/static', express.static(__dirname + '/public')); // need this to read bundle.js
 require('./server/routes')(app);
