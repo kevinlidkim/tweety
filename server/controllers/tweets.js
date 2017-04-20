69995264b00f5396b0cd11cabc23d53a6c17334d
kevin <kevin.li.3@stonybrook.edu>
2017-04-19 21:21:27 -0400

    why is it not getting through

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -870,6 +870,7 @@ exports.delete_item = function(req, res) {
   })
     .then(tweet => {
       if (tweet.lastErrorObject.n > 0) {
+        console.log('Deleted tweet with id ' + tweet.value._id);
         if (tweet.value.media && tweet.value.media.length > 0) {
           console.log('Deleting media with file_id = ' + tweet.value.media);
           var query = 'DELETE FROM media WHERE file_id = ?';
