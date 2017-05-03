ea48d078cc63d5bb8eb5319edc1741384e31b11f
kevin <kevin.li.3@stonybrook.edu>
2017-05-02 20:05:56 -0400

    changed cassandra contact

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -5,7 +5,7 @@ var moment = require('moment');
 
 var cassandra = require('cassandra-driver');
 // var client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'tweety' });
-var client = new cassandra.Client({ contactPoints: ['192.168.1.21'], keyspace: 'tweety' });
+var client = new cassandra.Client({ contactPoints: ['192.168.1.38'], keyspace: 'tweety' });
 
 var multer = require('multer');
 var upload = multer().single('content');
